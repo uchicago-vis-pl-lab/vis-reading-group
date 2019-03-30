cc7c281c0ffcb9538a816f0691749c320c2d9acd
Andrew McNutt <mcnutt.andrew@gmail.com>
2019-03-30 13:25:27 -0500

    reading for first half of spring quarter

--- src/components/constants.js
+++ src/components/constants.js
@@ -32,7 +32,60 @@ export const history = [{
   presenter: 'Andrew',
   quarter: 'Winter 2019'
 }, {
-  paper: 'TBD',
+  date: '4/5/19',
+  paper: 'An Information-theoretic Framework for Visualization',
+  authors: 'Min Chen and Heike Jaenicke',
+  venue: 'VIS 2010',
+  link: 'https://cs.brynmawr.edu/Courses/cs380/fall2012/ChenJanicke2010.pdf',
+  presenter: 'Teo',
+  quarter: 'Spring 2019'
+}, {
+  date: '4/12/19',
+  paper: 'An Algebraic Process for Visualization Design',
+  authors: 'Gordon Kindlmann and Carlos Scheidegger',
+  venue: 'VIS 2014',
+  link: 'http://vis.cs.ucdavis.edu/vis2014papers/TVCG/papers/2181_20tvcg12-kindlmann-2346325.pdf',
+  presenter: 'Will',
+  quarter: 'Spring 2019'
+}, {
+  date: '4/19/19',
+  paper: 'Understanding Visualization: A Formal Approach Using Category Theory and Semiotics',
+  authors: 'Paul Vickers, Joe Faith, and Nick Rossiter',
+  venue: 'DECISIVe 2017',
+  link: 'https://pdfs.semanticscholar.org/f2c6/40b88265071c7fc2e148871d73845b8b0705.pdf',
+  presenter: 'Andrew',
+  quarter: 'Spring 2019'
+}, {
+  date: '4/26/19',
+  paper: 'Critical InfoVis: exploring the politics of visualization',
+  authors: 'Marian Dörk, Christopher Collins, Patrick Feng, and Sheelagh Carpendale',
+  venue: 'AltCHI 2013',
+  link: 'https://mariandoerk.de/criticalinfovis/',
+  presenter: 'Miranda',
+  quarter: 'Spring 2019'
+}, {
+  date: '5/3/19',
+  paper: 'Human-Computer Insurrection Notes on an Anarchist HCI',
+  authors: 'Os Keyes, Josephine Hoy, and Margaret Drouhard',
+  venue: 'CHI 2019',
+  link: 'https://ironholds.org/resources/papers/anarchist_hci.pdf',
+  presenter: 'Galen',
+  quarter: 'Spring 2019'
+}, {
+  date: '5/10/19',
+  paper: 'What May Visualization Processes Optimize?',
+  authors: 'Min Chen, Amos Golan',
+  venue: 'VIS 2016',
+  link: 'https://arxiv.org/abs/1506.02245',
+  presenter: 'Nick',
+  quarter: 'Spring 2019'
+}, {
+  date: '5/17/19',
+  paper: 'Quality Metrics for Information Visualization',
+  authors: 'M. Behrisch, M. Blumenschein, N. W. Kim, L. Shao, M. El-Assady, J. Fuchs, D. Seebacher, A. Diehl U. Brandes, H. Pfister, T. Schreck, and D. Weiskopf, D. A. Keim',
+  venue: 'EuroVis 2018',
+  link: 'https://bib.dbvis.de/uploadedFiles/QMSTAR_QualityMetricsForInformationVisualization_FINAL.pdf',
+  presenter: '????',
   quarter: 'Spring 2019'
 }].sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
 /* eslint-enable max-len*/
@@ -44,7 +97,7 @@ export const quarterInfo = {
     order: 0
   },
   'Spring 2019': {
-    location: 'Location TBD',
+    location: 'JCL 354',
     time: 'Fridays 12:30',
     order: 1
   }
